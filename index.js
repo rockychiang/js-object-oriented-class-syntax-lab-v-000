8cf9f672e2b3d346cb5ad591dfa9c2c64407f373
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 07:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ class BoardMember {
     `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+}
